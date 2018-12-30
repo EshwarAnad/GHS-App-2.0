c3a159c0924c127752dcdfdec2e14779c06488bd
Jacoby Broadnax <32787662+MashPop@users.noreply.github.com>
2018-12-30 14:20:57 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -2,6 +2,16 @@
 
   var close = document.getElementsByClassName("close");
 
+  var livesect = document.getElementsByClassName("live-sect");
+
+  var videosect = document.getElementsByClassName("video-sect");
+
+  var topnavbar = document.getElementsByClassName("top-navigation")
+
+  var homescreen = document.getElementsByClassName("homescreen")
+
+  var mediasect = document.getElementsByClassName("media-sect")
+
   function expand() {
       hammenu[0].style.display = "block";
       close[0].style.display = "block";
@@ -10,4 +20,21 @@
   function collapse() {
       hammenu[0].style.display = "none";
       close[0].style.display = "none";
+  }
+
+  function video() {
+      livesect[0].style.display = "none";
+      videosect[0].style.display = "block"
+
+  }
+
+  function live() {
+      livesect[0].style.display = "block";
+      videosect[0].style.display = "none";
+  }
+
+  function leftpress() {
+      homescreen[0].style.display = "none";
+      topnavbar[0].style.boxShadow = "0px;"
+      mediasect[0].style.display = "block"
   }

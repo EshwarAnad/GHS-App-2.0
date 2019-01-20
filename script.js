8dbdbece458dc7479eb947404ee64bad5f3c265f
Jacoby Broadnax <32787662+MashPop@users.noreply.github.com>
2019-01-20 14:01:37 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -26,7 +26,7 @@
 
   var admiralnationsect = document.getElementsByClassName("admiralnation-sect");
 
-  var kahootsect = document.getElementsByClassName("kahoot-sect")
+  var kahootsect = document.getElementsByClassName("kahoot-sect");
 
   function expand() {
       hammenu[0].style.display = "block";
@@ -200,60 +200,19 @@
       }
   }
 
-  (function (window) {
-      'use strict';
-      window.code = window.code || {};
-      window.code.lightweightYoutubePlayer = function () {
-          var dataYoutubeVideos = '[data-youtube]';
-          var youtubeVideos = [...document.querySelectorAll(dataYoutubeVideos)];
 
-          function init() {
-              youtubeVideos.forEach(function (element) {
-                  bindYoutubeVideoEvent(element);
-              });
-          }
-
-          function bindYoutubeVideoEvent(element) {
-              var button = element.querySelector('[data-youtube-button]');
-              button.addEventListener('click', createIframe);
-          }
-
-          function createIframe(event) {
-              var url = event.target.dataset.youtubeButton;
-              var youtubePlaceholder = event.target.parentNode;
-              var htmlString = '<div class="video__youtube "> <iframe class="video__iframe " src=" ' + url + '?autoplay=1 " frameborder="0 " allowfullscreen></iframe></div>';
-              youtubePlaceholder.style.display = 'none';
-              youtubePlaceholder.insertAdjacentHTML('beforebegin', htmlString);
-              youtubePlaceholder.parentNode.removeChild(youtubePlaceholder);
-          }
-          return {
-              init: init
-          }
-      };
-  })(window)
-  ready();
-
-  function ready() {
-      var lightweightYoutubePlayer = new code.lightweightYoutubePlayer()
-      if (document.readyState != 'loading') {
-          page.init()
-      } else {
-          document.addEventListener('DOMContentLoaded', lightweightYoutubePlayer.init);
+  var lastTouchEnd = 0;
+  document.addEventListener('touchend', function (event) {
+      var now = (new Date()).getTime();
+      if (now - lastTouchEnd <= 300) {
+          event.preventDefault();
       }
-  }
+      lastTouchEnd = now;
+  }, false);
 
-  if ('serviceWorker' in navigator) {
-      window.addEventListener('load', function () {
-          navigator.serviceWorker.register('/sw.js').then(function (registration) {
-              // Registration was successful
-              console.log('ServiceWorker registration successful with scope: ', registration.scope);
-          }, function (err) {
-              // registration failed :(
-              console.log('ServiceWorker registration failed: ', err);
-          });
-      });
-  }
 
-  self.addEventListener('install', function (event) {
-      // Perform install steps
-  });
+  document.addEventListener('touchmove', function (event) {
+      if (event.scale !== 1) {
+          event.preventDefault();
+      }
+  }, false);
